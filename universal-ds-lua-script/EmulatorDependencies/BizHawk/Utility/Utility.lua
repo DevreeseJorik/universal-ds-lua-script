69fbc1aa4454e6db967d028884bc54299fb3dae7
Devreese Jorik <jorik.devreese@student.howest.be>
2022-12-28 13:38:08 +0100

    added pointer check

--- universal-ds-lua-script/EmulatorDependencies/BizHawk/Utility/Utility.lua
+++ universal-ds-lua-script/EmulatorDependencies/BizHawk/Utility/Utility.lua
@@ -4,3 +4,10 @@ function Utility:format(val,len)
     len = len or 1
     return string.format("%0"..len.."X", bit.band(4294967295, val))
 end
+
+function Utility:isPointer(addr,dom)
+    dom = dom or "main"
+    if dom == "main" then
+        return (addr >= 0x02000000) and (addr < 0x03000000)
+    end
+end 
