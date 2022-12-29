555e20acde680844c7e2e9fe97559313116dd96c
Devreese Jorik <jorik.devreese@student.howest.be>
2022-12-29 11:03:23 +0100

    disabled loadstring migration

--- universal-ds-lua-script/EmulatorDependencies/BizHawk/version_2.9-x/MigrationHelpers.lua
+++ universal-ds-lua-script/EmulatorDependencies/BizHawk/version_2.9-x/MigrationHelpers.lua
@@ -1,4 +1,4 @@
 bit = (require "migration_helpers").EmuHawk_pre_2_9_bit()
-if loadstring == nil then 
-    loadstring = load -- for lua 5.2+ compatibility
-end
+-- if loadstring == nil then 
+--     loadstring = load -- for lua 5.2+ compatibility
+-- end
