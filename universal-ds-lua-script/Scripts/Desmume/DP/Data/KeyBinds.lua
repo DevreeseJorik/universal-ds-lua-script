e4f268871197ff8d62b42d4fcd1d0619146f550c
Devreese Jorik <jorik.devreese@student.howest.be>
2022-12-31 02:21:52 +0100

    Changed loadstring to function references

--- universal-ds-lua-script/Scripts/Desmume/DP/Data/KeyBinds.lua
+++ universal-ds-lua-script/Scripts/Desmume/DP/Data/KeyBinds.lua
@@ -1,10 +1,10 @@
 keyBinds = {
     keyConfig = {
-        ["LoadLines:toggleLoadLines"] = {"shift","L"},
-        ["LoadLines:toggleGridLines"] = {"shift","G"},
-        ["LoadLines:toggleChunkLines"] = {"shift","C"},
-        ["LoadLines:toggleMapLines"] = {"shift","K"},
-        ["Chunks:toggleChunks"] = {"shift","M"}
+        {keys = {"shift","L"}, func = function() LoadLines:toggleLoadLines() end},
+        {keys = {"shift","G"}, func = function() LoadLines:toggleGridLines() end},
+        {keys = {"shift","C"}, func = function() LoadLines:toggleChunkLines() end},
+        {keys = {"shift","K"}, func = function() LoadLines:toggleMapLines() end},
+        {keys = {"shift","M"}, func = function() Chunks:toggleChunks() end},
     },
 
     keyConfigContinues = {
