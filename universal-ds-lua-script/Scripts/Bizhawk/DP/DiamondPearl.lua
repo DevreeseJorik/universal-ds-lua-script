a578265a1169b79bc8e02e6e2677269d2346faf4
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-18 21:08:38 +0100

    updated repositories

--- universal-ds-lua-script/Scripts/Bizhawk/DP/DiamondPearl.lua
+++ universal-ds-lua-script/Scripts/Bizhawk/DP/DiamondPearl.lua
@@ -9,6 +9,9 @@ function Game:init()
     ChunkData = ChunkData:new(0x229F0)
     Chunks = Chunks:new()
 
+    MapData = MapData:new(0x22A84)
+    Maps = Maps:new(558)
+
     ScriptHandler = ScriptHandler:new()
     ScriptData = ScriptData:new()
 
@@ -17,35 +20,53 @@ end
 
 function Game:importFiles()
     self.templateDir = self.templateDir .. "/Gen" .. self.generation
-    dofile(self.templateDir .. "/Data/ChunkData.lua")
-    dofile(self.templateDir .. "/Data/PlayerData.lua")
-    dofile(self.templateDir .. "/Data/KeyBinds.lua")
-    dofile(self.dataDir .. "/KeyBinds.lua") -- extend keybinds with script specific keybinds
-    dofile(self.templateDir .. "/Repositories/LoadLines.lua")
+    
     dofile(self.templateDir .. "/Repositories/MemoryState.lua")
+
+    dofile(self.templateDir .. "/Data/PlayerData.lua")
+    dofile(self.templateDir .. "/Repositories/LoadLines.lua")
+    
+    dofile(self.templateDir .. "/Data/ChunkData.lua")
     dofile(self.templateDir .. "/Repositories/Chunks.lua")
+
+    dofile(self.templateDir .. "/Data/MapData.lua")
+    dofile(self.templateDir .. "/Repositories/Maps.lua")
 
     dofile(self.dataDir .. "/ScriptData.lua")
     dofile(self.dir .. "/Repositories/ScriptHandler.lua")
 
+    dofile(self.templateDir .. "/Data/KeyBinds.lua")
+    dofile(self.dataDir .. "/KeyBinds.lua") -- extend keybinds with script specific keybinds
+
     dofile(self.templateDir .. "/Repositories/Cheats.lua")
     dofile(self.dataDir .. "/Cheats.lua") -- includes addresses
 end
+
+updateFrame = 0
 
 function Game:main()
     MemoryState:update() -- always execute this first to get base pointer
     Display:update()
     PlayerData:update()
     ChunkData:update()
+    MapData:update()
     ScriptData:update()
 
     Input:runChecks()
 
     MemoryState:display()
     ScriptHandler:display()
-    LoadLines:display()
-    Chunks:display()
+    
+    Maps:display()
     ScriptHandler:display()
+
+    if (updateFrame % 4 == 0) then
+        LoadLines:display()
+        Chunks:display()
+    end
+    Chunks:showDrawCount()
+
     self:displayGameInfo()
+    updateFrame = updateFrame + 1
 end
 
