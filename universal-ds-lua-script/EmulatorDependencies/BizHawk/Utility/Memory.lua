8c8f0a8588a6aad7328bd2d744dafa4a206903e3
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-24 22:39:08 +0100

    Added little endian support

--- universal-ds-lua-script/EmulatorDependencies/BizHawk/Utility/Memory.lua
+++ universal-ds-lua-script/EmulatorDependencies/BizHawk/Utility/Memory.lua
@@ -100,12 +100,22 @@ function Memory:write_bytes_as_array(addr, val)
     mainmemory.write_bytes_as_array(addr-0x2000000, val)
 end
 
-function Memory:getByteRange(table,index, size)
+function Memory:getByteRange_be(table,index, size)
     local shift = (size - 1) * 8
     local value = 0
     for i = index, index + size - 1 do
         value = bit.bor(value, bit.lshift(tonumber(table[i+1]), shift))
         shift = shift - 8
+    end
+    return value
+end
+
+function Memory:getByteRange(table, index, size)
+    local shift = 0
+    local value = 0
+    for i = index, index + size - 1 do
+        value = bit.bor(value, bit.lshift(tonumber(table[i+1]), shift))
+        shift = shift + 8
     end
     return value
 end
