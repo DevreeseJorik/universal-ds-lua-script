76109518e0bd06ec15248067024cbe32af35b071
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-24 22:58:34 +0100

    Added more trigger types

--- universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/EventTriggers.lua
+++ universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/EventTriggers.lua
@@ -1,5 +1,16 @@
 EventTriggers = {
-
+    talkTriggersColors = {
+        background = 0xFFFF0000,
+        border = 0xFFFF0000
+    },
+    warpTriggersColors = {
+        background = 0xFF00FF00,
+        border = 0xFF00FF00
+    },
+    walkTriggersColors = {
+        background = 0xFF0000FF,
+        border = 0xFF0000FF
+    },
 }
 
 function EventTriggers:new(o)
@@ -15,10 +26,14 @@ function EventTriggers:display()
     local y_cam = NPCstruct.y_cam_16
     local z_cam = NPCstruct.z_cam_16
     local table = EventTriggersData.talkTriggers
-    self:displayEventTriggers(table,x_cam,y_cam,z_cam)
+    self:displayEventTriggers(table,x_cam,y_cam,z_cam,self.talkTriggersColors)
+    table = EventTriggersData.warpTriggers
+    self:displayEventTriggers(table,x_cam,y_cam,z_cam,self.warpTriggersColors)
+    table = EventTriggersData.walkTriggers
+    self:displayWalkTriggers(table,x_cam,y_cam,z_cam,self.walkTriggersColors)
 end
 
-function EventTriggers:displayEventTriggers(table,x,y,z)
+function EventTriggers:displayEventTriggers(table,x,y,z,colors)
     local trigger, x_trigger, y_trigger, z_trigger
     local w = 16
     local h = 14
@@ -35,11 +50,39 @@ function EventTriggers:displayEventTriggers(table,x,y,z)
             centerY + (z_trigger - z)*h, 
             w,
             h, 
-            0xFFFF0000, 
-            0xFFFF0000
+            colors.background,
+            colors.border
         )
     end
 
 end
 
 
+function EventTriggers:displayWalkTriggers(table,x,y,z,colors)
+    local trigger, x_trigger, y_trigger, z_trigger
+    local w = 16
+    local h = 14
+    local centerX = Display.left + Display.width/2 - w/2
+    local centerY = Display.height/2 - h/2
+
+    for i=1, #table do
+        trigger = table[i]
+        x_trigger = trigger.x
+        z_trigger = trigger.z
+        numTriggersHorizontal = trigger.numTriggersHorizontal
+        numTriggersVertical = trigger.numTriggersVertical
+        for j=0, numTriggersHorizontal-1 do
+            for k=0, numTriggersVertical-1 do
+                gui.drawRectangle(
+                    centerX + (x_trigger - x)*w + j*w, 
+                    centerY + (z_trigger - z)*h + k*h, 
+                    w,
+                    h, 
+                    colors.background,
+                    colors.border
+                )
+            end
+        end
+    end
+
+end
