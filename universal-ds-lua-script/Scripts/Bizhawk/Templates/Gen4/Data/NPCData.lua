d4692cbf3d4e4b6bf80f6a8f83172487321bc761
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-26 22:51:24 +0100

    /bugfix now starts index at 1

--- universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Data/NPCData.lua
+++ universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Data/NPCData.lua
@@ -65,48 +65,48 @@ function NPCData:update()
     if (self.NPCCount < 0) or (self.maxNPCCount ~= 64) or (self.NPCCount > self.maxNPCCount) then 
         return 
     end
-
+    
     local NPCMemory = Memory.read_bytes_as_array(self.NPCAddress, self.NPCSize * self.NPCCount)
 
     for i = 0, self.NPCCount - 1 do
-        self.NPCs[i] = NPC:new()
-        self.NPCs[i].spriteId = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x10, 0x2)
-        self.NPCs[i].ObjCode = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x12, 0x2)
-        self.NPCs[i].moveCode = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x14, 0x2)
-        self.NPCs[i].eventType = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x16, 0x2)
-        self.NPCs[i].eventFlag = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x18, 0x2)
+        self.NPCs[i+1] = NPC:new()
+        self.NPCs[i+1].spriteId = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x10, 0x2)
+        self.NPCs[i+1].ObjCode = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x12, 0x2)
+        self.NPCs[i+1].moveCode = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x14, 0x2)
+        self.NPCs[i+1].eventType = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x16, 0x2)
+        self.NPCs[i+1].eventFlag = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x18, 0x2)
 
-        self.NPCs[i].eventId = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x20, 0x2)
-        self.NPCs[i].movement = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x24, 0x4)
-        self.NPCs[i].facingDirection = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x28, 0x4)
-        self.NPCs[i].lastMovement = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x2C, 0x4)
-        self.NPCs[i].lastFacingDirection = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x30, 0x4)
+        self.NPCs[i+1].eventId = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x20, 0x2)
+        self.NPCs[i+1].movement = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x24, 0x4)
+        self.NPCs[i+1].facingDirection = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x28, 0x4)
+        self.NPCs[i+1].lastMovement = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x2C, 0x4)
+        self.NPCs[i+1].lastFacingDirection = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x30, 0x4)
 
-        self.NPCs[i].xSpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x4C, 0x4)
-        self.NPCs[i].ySpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x50, 0x4)
-        self.NPCs[i].zSpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x54, 0x4)
+        self.NPCs[i+1].xSpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x4C, 0x4)
+        self.NPCs[i+1].ySpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x50, 0x4)
+        self.NPCs[i+1].zSpawn = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x54, 0x4)
 
-        self.NPCs[i].xFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x58, 0x4)
-        self.NPCs[i].yFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x5C, 0x4)
-        self.NPCs[i].zFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x60, 0x4)
+        self.NPCs[i+1].xFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x58, 0x4)
+        self.NPCs[i+1].yFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x5C, 0x4)
+        self.NPCs[i+1].zFinal = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x60, 0x4)
 
-        self.NPCs[i].xPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x64, 0x4)
-        self.NPCs[i].yPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x68, 0x4)
-        self.NPCs[i].zPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x6C, 0x4)
+        self.NPCs[i+1].xPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x64, 0x4)
+        self.NPCs[i+1].yPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x68, 0x4)
+        self.NPCs[i+1].zPhysical = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x6C, 0x4)
 
-        self.NPCs[i].xCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x70, 0x2)
-        self.NPCs[i].xCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x72, 0x2)
-        self.NPCs[i].yCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x74, 0x2)
-        self.NPCs[i].yCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x76, 0x2)
-        self.NPCs[i].zCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x78, 0x2)
-        self.NPCs[i].zCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x7A, 0x2)
+        self.NPCs[i+1].xCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x70, 0x2)
+        self.NPCs[i+1].xCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x72, 0x2)
+        self.NPCs[i+1].yCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x74, 0x2)
+        self.NPCs[i+1].yCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x76, 0x2)
+        self.NPCs[i+1].zCamSubpixel = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x78, 0x2)
+        self.NPCs[i+1].zCam = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0x7A, 0x2)
 
-        self.NPCs[i].x = self.NPCs[i].xCam
-        self.NPCs[i].y = self.NPCs[i].yCam
-        self.NPCs[i].z = self.NPCs[i].zCam
+        self.NPCs[i+1].x = self.NPCs[i+1].xCam
+        self.NPCs[i+1].y = self.NPCs[i+1].yCam
+        self.NPCs[i+1].z = self.NPCs[i+1].zCam
 
-        self.NPCs[i].tileId1 = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0xCC, 0x2)
-        self.NPCs[i].tileId2 = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0xCE, 0x2)
+        self.NPCs[i+1].tileId1 = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0xCC, 0x2)
+        self.NPCs[i+1].tileId2 = Memory:getByteRange(NPCMemory, i * self.NPCSize + 0xCE, 0x2)
     end
 
 end
