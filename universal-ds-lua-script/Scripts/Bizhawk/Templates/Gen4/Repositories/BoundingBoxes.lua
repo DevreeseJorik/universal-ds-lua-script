4369e0969b486d482cdf02580aa81ef73c34e7ab
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-26 18:25:55 +0100

    Moved object display to bounding boxes

--- universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/BoundingBoxes.lua
+++ universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/BoundingBoxes.lua
@@ -32,3 +32,42 @@ function BoundingBoxes:display()
     EventTriggers:display()
     self:displayPlayer()
 end
+
+function BoundingBoxes:displayObjects(table,x,y,z,colors)
+    local object
+    local w = 16
+    local h = 14
+    local foreshortenX = math.cos(math.rad(0))
+    local foreshortenY = math.sin(math.rad(60))
+    local centerX = Display.left + Display.width/2 - w/2 * foreshortenX
+    local centerY = Display.height/2 - h/2 * foreshortenY
+
+    for i=1, #table do
+        object = table[i]
+        xObj = object.x
+        zObj = object.z
+
+        self:displayObject(
+            centerX + (xObj - x)*w*foreshortenX, 
+            centerY + (zObj - z)*h*foreshortenY, 
+            w*foreshortenX,
+            h*foreshortenY, 
+            colors
+        )
+    end
+end
+
+function BoundingBoxes:displayObject(x,y,w,h,colors)
+    -- local left = Display.left
+    -- local right = left + Display.width
+    -- local top = Display.top
+    -- local bottom = Display.height
+    -- if (x + w) < left or x > right or (y + h) < top or y > bottom then
+    --     return
+    -- end
+    -- x = math.max(x, left)
+    -- y = math.max(y, top)
+    -- w = math.min(w, right - x)
+    -- h = math.min(h, bottom - y)
+    gui.drawRectangle(x, y, w, h, colors.border, colors.background)
+end
