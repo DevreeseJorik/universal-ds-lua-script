bc84cb389f2ce42cd57765ecbfd3f274ff4f3445
Devreese Jorik <jorik.devreese@student.howest.be>
2023-02-26 18:25:17 +0100

    moved code to bounding boxes

--- universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/EventTriggers.lua
+++ universal-ds-lua-script/Scripts/Bizhawk/Templates/Gen4/Repositories/EventTriggers.lua
@@ -1,15 +1,15 @@
 EventTriggers = {
     talkTriggersColors = {
-        border = 0x80FF00FF,
-        background = 0xFFFF00FF
+        background = 0x80FF00FF,
+        border= 0xFFFF00FF
     },
     warpTriggersColors = {
-        border = 0x80FF0000,
-        background = 0xFFFF0000
+        background = 0x80FF0000,
+        border= 0xFFFF0000
     },
     walkTriggersColors = {
-        border = 0x80FFFF00,
-        background = 0xFFFFFF00
+        background = 0x80FFFF00,
+        border= 0xFFFFFF00
     },
 }
 
@@ -24,40 +24,10 @@ function EventTriggers:display()
     local xCam = PlayerData.xCam
     local yCam = PlayerData.yCam
     local zCam = PlayerData.zCam
-    local table = EventTriggersData.talkTriggers
-    self:displayEventTriggers(table,xCam,yCam,zCam,self.talkTriggersColors)
-    table = EventTriggersData.warpTriggers
-    self:displayEventTriggers(table,xCam,yCam,zCam,self.warpTriggersColors)
-    table = EventTriggersData.walkTriggers
-    self:displayWalkTriggers(table,xCam,yCam,zCam,self.walkTriggersColors)
+    BoundingBoxes:displayObjects(EventTriggersData.talkTriggers,xCam,yCam,zCam,self.talkTriggersColors)
+    BoundingBoxes:displayObjects(EventTriggersData.warpTriggers,xCam,yCam,zCam,self.warpTriggersColors)
+    self:displayWalkTriggers(EventTriggersData.walkTriggers,xCam,yCam,zCam,self.walkTriggersColors)
 end
-
-function EventTriggers:displayEventTriggers(table,x,y,z,colors)
-    local trigger, xTrigger, yTrigger, zTrigger
-    local w = 16
-    local h = 14
-    local foreshortenX = math.cos(math.rad(0))
-    local foreshortenY = math.sin(math.rad(60))
-    local centerX = Display.left + Display.width/2 - w/2 * foreshortenX
-    local centerY = Display.height/2 - h/2 * foreshortenY
-
-    for i=1, #table do
-        trigger = table[i]
-        xTrigger = trigger.x
-        zTrigger = trigger.z
-
-        gui.drawRectangle(
-            centerX + (xTrigger - x)*w*foreshortenX, 
-            centerY + (zTrigger - z)*h*foreshortenY, 
-            w*foreshortenX,
-            h*foreshortenY, 
-            colors.background,
-            colors.border
-        )
-    end
-
-end
-
 
 function EventTriggers:displayWalkTriggers(table,x,y,z,colors)
     local trigger, xTrigger, yTrigger, zTrigger
@@ -76,13 +46,12 @@ function EventTriggers:displayWalkTriggers(table,x,y,z,colors)
         numTriggersVertical = trigger.numTriggersVertical
         for j=0, numTriggersHorizontal-1 do
             for k=0, numTriggersVertical-1 do
-                gui.drawRectangle(
-                    centerX + (xTrigger - x)*w*foreshortenX + j*w*foreshortenX,
-                    centerY + (zTrigger - z)*h*foreshortenY + k*h*foreshortenY, 
+                BoundingBoxes:displayObject(
+                    centerX + (xTrigger + j - x)*w*foreshortenX, 
+                    centerY + (zTrigger + k - z)*h*foreshortenY, 
                     w*foreshortenX,
                     h*foreshortenY, 
-                    colors.background,
-                    colors.border
+                    colors
                 )
             end
         end
